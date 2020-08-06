327ec8665fc010f1ea3f238d86b8d2bc2d238c2c
hermy.halim@prudential.co.id <hermy.halim@prudential.co.id>
2020-08-06 19:11:44 +0700

    update my work

--- app.py
+++ app.py
@@ -8,43 +8,102 @@ import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
+class Data:
+  def __init__(self, title, year, rating, votes, metascore):
+    self.title = title
+    self.year = year
+    self.rating = rating
+    self.votes = votes
+    self.metascore = metascore
+
+  def to_dict(self):
+    return {
+        'Title': self.title,
+        'Year': self.year,
+        'Rating': self.rating,
+        'Votes': self.votes,
+        'Metascore': self.metascore
+    }
+
 def scrap(url):
     #This is fuction for scrapping
-    url_get = requests.get(url)
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+                'Cache-Control': 'no-cache'}   
+
+    url_get = requests.get(url,headers=headers)
     soup = BeautifulSoup(url_get.content,"html.parser")
     
     #Find the key to get the information
-    table = soup.find(___) 
-    tr = table.find_all(___) 
-
+    table = soup.find("div",{"class":"lister-list"}) 
+    tr = table.find_all("div",{"class":"lister-item mode-advanced"}) 
     temp = [] #initiating a tuple
 
-    for i in range(1, len(tr)):
-        row = table.find_all('tr')[i]
+    for i in range(0, len(tr)):
+         
+        row = tr[i]
         #use the key to take information here
-        #name_of_object = row.find_all(...)[0].text
+        # title class
+        titleH3=row.find("h3",{"class":"lister-item-header"})
+        title=titleH3.find("a").text
+        year=titleH3.find("span",{"class":"lister-item-year text-muted unbold"}).text
 
+        rating=0
+        metascore=0
+        ratingTag = row.find("div",{"class":"ratings-bar"})
+        if ratingTag is not None:
+            ratingsBarTag = ratingTag.find("strong")
+            if ratingsBarTag is not None:
+                rating = ratingsBarTag.text
+            
+            metascoreTag = ratingTag.find("span",{"class":"metascore"})
+            if metascoreTag is not None:
+                metascore = metascoreTag.text.strip()
+        
+        votes=0
+        votesTag = row.find("p",{"class":"sort-num_votes-visible"})
+        if votesTag is not None:
+            votesText = votesTag.find("span",{"class":"text-muted"}).text
+            if votesText == 'Votes:':
+                votes = votesTag.find("span",{"name":"nv"})['data-value']
 
-
-
-
-
-        temp.append((___)) #append the needed information 
+        d= Data(title,year,rating,votes,metascore)
+        temp.append(d) #append the needed information 
     
-    temp = temp[::-1] #remove the header
-
-    df = pd.DataFrame(temp, columns = (___)) #creating the dataframe
-   #data wranggling -  try to change the data type to right data type
-
+    
+    df= pd.DataFrame.from_records([d.to_dict() for d in temp])   #data wranggling -  try to change the data type to right data type
+    if (df.size>0) :
+        df['Rating'] = df['Rating'].astype('float64')
+        df['Votes'] = df['Votes'].astype('int64')
+        df['Metascore'] = df['Metascore'].astype('int64')
    #end of data wranggling
-
-    return df
+    nextUrlTag = soup.find("div",{"class":"desc"}).find("a",{"class":"lister-page-next next-page"}) 
+    nextUrl=''
+    if nextUrlTag is not None:
+        nextUrl=nextUrlTag['href']
+    return [df,nextUrl]
 
 @app.route("/")
 def index():
-    df = scrap(___) #insert url here
+    returnData = scrap('https://www.imdb.com/search/title/?release_date=2019-01-01,2019-12-31')
+    df=returnData[0] #insert url here
+    result=''
+    pageCount=0
+    nextUrl = returnData[1]
+    while ((nextUrl != '') and (pageCount < 10)):
+        print(nextUrl)
+        returnData2 = scrap('https://www.imdb.com'+nextUrl)
+        df2 = returnData2[0]
+        nextUrl = returnData2[1]
+        pageCount=pageCount+1
+        if df2.size>0:
+            df = df.append(df2, ignore_index=True)
 
-    #This part for rendering matplotlib
+    # df = pd.read_pickle("./cached_dataframe200.pkl")
+    # df['Metascore'] = df['Metascore'].astype('int64')
+    print(df.dtypes)
+    
+
+    # This part for rendering matplotlib
     fig = plt.figure(figsize=(5,2),dpi=300)
     df.plot()
     
@@ -57,10 +116,51 @@ def index():
     result = str(figdata_png)[2:-1]
     #This part for rendering matplotlib
 
+
+    fig = plt.figure(figsize=(5,5),dpi=300)
+    df.set_index('Title')['Rating'].sort_values(ascending=False).head(7).plot.bar()
+    
+    #Do not change this part
+    plt.savefig('plot2',bbox_inches="tight") 
+    plt.tight_layout()
+    figfile = BytesIO()
+    plt.savefig(figfile, format='png')
+    figfile.seek(0)
+    figdata_png = base64.b64encode(figfile.getvalue())
+    result2 = str(figdata_png)[2:-1]
+    #This part for rendering matplotlib
+
+    fig = plt.figure(figsize=(5,5),dpi=300)
+    df.set_index('Title')['Votes'].sort_values(ascending=False).head(7).plot.bar()
+    
+    #Do not change this part
+    plt.savefig('plot2',bbox_inches="tight") 
+    plt.tight_layout()
+    figfile = BytesIO()
+    plt.savefig(figfile, format='png')
+    figfile.seek(0)
+    figdata_png = base64.b64encode(figfile.getvalue())
+    result3 = str(figdata_png)[2:-1]
+    #This part for rendering matplotlib
+
+    fig = plt.figure(figsize=(5,5),dpi=300)
+    df.set_index('Title')['Metascore'].sort_values(ascending=False).head(7).plot.bar()
+    
+    #Do not change this part
+    plt.savefig('plot2',bbox_inches="tight") 
+    plt.tight_layout()
+    figfile = BytesIO()
+    plt.savefig(figfile, format='png')
+    figfile.seek(0)
+    figdata_png = base64.b64encode(figfile.getvalue())
+    result4 = str(figdata_png)[2:-1]
+    #This part for rendering matplotlib
+
     #this is for rendering the table
+    # df.to_pickle('cached_dataframe200.pkl')
     df = df.to_html(classes=["table table-bordered table-striped table-dark table-condensed"])
 
-    return render_template("index.html", table=df, result=result)
+    return render_template("index.html", table=df, result=result, result2=result2, result3=result3, result4=result4)
 
 
 if __name__ == "__main__": 
